56f67ba9829ff65923dcce99e060319ce1ec2710
Guilherme Uriarte <guiuriartedev@gmail.com>
2026-03-23 20:45:36 -0300

    fix: jwt registerAsync expiresIn type cast

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         secret: config.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: config.get<string>('JWT_EXPIRES_IN') || '15m' },
+        signOptions: { expiresIn: (config.get<string>('JWT_EXPIRES_IN') || '15m') as any },
       }),
     }),
   ],
